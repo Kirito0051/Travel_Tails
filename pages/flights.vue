3450733f203561e68ac0cd1a3ba657bdbda539a8
Kirito0051 <anas0066@gmail.com>
2025-01-02 09:25:56 +0530

    Update flights.vue

--- pages/flights.vue
+++ pages/flights.vue
@@ -1,7 +1,15 @@
 <template>
-    <section class="container mx-auto py-12">
-        <h2 class="text-3xl font-bold mb-6">Find Your Flights</h2>
-        <form class="bg-gray-100 p-6 rounded shadow-md space-y-4">
+    <section class="container mx-auto py-4">
+        <!-- Attention-Grabbing Image -->
+        <div class="relative mb-8 h-[30rem]">
+            <img src="/public/images/plane_bg.jpg" alt="Scenic view from a plane during flight"
+                class="w-full h-full object-cover rounded-3xl" />
+        </div>
+
+        <h2 class="text-2xl sm:text-3xl md:text-4xl font-bold text-center mb-6">
+            Find Your Flights
+        </h2>
+        <form class="bg-gray-100 p-10 rounded shadow-md space-y-4">
             <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
                 <input type="text" placeholder="Departure City" class="p-4 border rounded" />
                 <input type="text" placeholder="Destination City" class="p-4 border rounded" />
@@ -10,9 +18,11 @@
                 <input type="date" class="p-4 border rounded" />
                 <input type="number" placeholder="Passengers" class="p-4 border rounded" />
             </div>
-            <button type="submit" class="bg-blue-600 hover:bg-blue-800 text-white px-6 py-3 rounded w-full">
-                Search Flights
-            </button>
+            <div class="flex justify-center">
+                <button type="submit" class="bg-blue-600 hover:bg-blue-800 text-white px-6 py-3 rounded w-48">
+                    Search Flights
+                </button>
+            </div>
         </form>
     </section>
 </template>
@@ -20,5 +30,5 @@
 <script setup lang="ts">
 definePageMeta({
     layout: 'custom'
-})
+});
 </script>
